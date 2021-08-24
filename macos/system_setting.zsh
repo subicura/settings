372fd70510312746847786adafbfd1e56f2f2c64
subicura <subicura@subicura.com>
2021-08-25 08:29:34 +0900

    update settings

--- macos/system_setting.zsh
+++ macos/system_setting.zsh
@@ -1,12 +1,30 @@
 #!/usr/bin/env zsh
 
+logger() {
+  local variant="${1:-info}"
+  local message="${2:-}"
+
+  if [ -z "$message" ]; then
+    return
+  fi
+
+  case "$variant" in
+    info)
+      printf "\033[34m%-8s\033[0m %s\n" "info" "$message"
+      ;;
+    success)
+      printf "\033[32m%-8s\033[0m %s\n" "success" "$message"
+      ;;
+    warning)
+      printf "\033[33m%-8s\033[0m %s\n" "warning" "$message"
+      ;;
+    error)
+      printf "\033[31m%-8s\033[0m %s\n" "error" "$message"
+      ;;
+  esac
+}
+
 () {
-  SCRIPT_DIR=$(cd $(dirname "${(%):-%x}") && pwd)
-
-  for libraryFile ($SCRIPT_DIR/../lib/*.zsh); do
-    source $libraryFile
-  done
-
   logger "info" "Setting macOS system ..."
   
   ## Dock & Menu Bar > Automatically hide and show the Dock (enable)
